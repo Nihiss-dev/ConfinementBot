2d3356b1cbfdb98c10e930f401a2a1aa76e1f3d0
Alexis FELDMAR <alexisf@Backlight.local>
2021-03-24 23:17:34 +0100

    Connecting with token

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+// This line MUST be first, for discord.js to read the process envs!
+require('dotenv').config(); 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -8,17 +10,27 @@ client.on('ready', () => {
 	});
 
 client.on('message', msg => {
-	if (msg.content.substring(0,1) == '!')
-	{
-		var args = msg.content.substring(1).split(' ');
-		var cmd = args[0];
-		
-		args = args.splice(1);
-		switch (cmd)
-		{
-		}
+	if (message.author.bot) return;
+	// The process.env.PREFIX is your bot's prefix in this case.
+	if (message.content.indexOf(process.env.PREFIX) !== 0) return;
+  
+	// This is the usual argument parsing we love to use.
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+  
+	// And our 2 real basic commands!
+	if(command === 'ping') {
+	  message.channel.send('Pong!');
+	} else
+	if (command === 'blah') {
+	  message.channel.send('Meh.');
 	}
-});
+  });
+
+
+// There's zero need to put something here. Discord.js uses process.env.CLIENT_TOKEN if it's available,
+// and this is what is being used here. If on discord.js v12, it's DISCORD_TOKEN
+client.login();
 
 client.on('voiceStateUpdate', (oldState, newState) => {
     if (!newState.channel || !newState.member) return; // Triggered if the user left a channel
@@ -28,5 +40,3 @@ client.on('voiceStateUpdate', (oldState, newState) => {
         if (!newState.member.roles.cache.has(role)) newState.member.roles.add(role); // Add the role to the user if they don't already have it
     }
 });
-client.login("ODI0MzQzNTIwMjk0ODYyOTE5.YFt_ug.ICucTKtG3EWYVmvdYh9sOZmfvvw");
-//client.login(process.env.DISCORD_TOKEN);
